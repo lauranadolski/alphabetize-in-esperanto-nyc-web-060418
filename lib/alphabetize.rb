24e91933c94c0ba6c99ea162256c935752545244
github username <github email address>
2018-05-28 14:22:08 -0700

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,11 @@
-def alphabetize(arr)
-  # code here
+def alphabetize(string_array)
+  sorted_array = string_array
+  esperanto_alphabet_array = ["a", "b", "c", "ĉ", "d", "e", "f", "g", "ĝ", "h", "ĥ", "i", "j", "ĵ", "k", "l", "m", "n", "o", "p", "r", "s", "ŝ", "t", "u", "ŭ", "v", "z"]
+  sorted_array.sort_by! do |string|
+    characters = string.chars
+    characters.collect do |letter|
+      esperanto_alphabet_array.index(letter)
+    end
+  end
+  return sorted_array
 end
